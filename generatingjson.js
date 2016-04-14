0fbe019c28d02229a450791cd7959c33f04f2895
ferhixo <callvik@kth.se>
2016-04-14 15:21:33 +0200

    Generating random utilization data, in a very cool way!

--- generatingjson.js
+++ generatingjson.js
@@ -6,14 +6,21 @@ var roomids = parsedJson.features.map(function(item){
 
 var humidityList = [100];
 var tempList = [100];
+var utilList = [100];
+
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function generate() {
   var date = new Date();
   var obj;
-  var roomid = roomids[getRandomInt(0, roomids.length-1)];
+  var roomid = roomids[getRandomIntInclusive(0, roomids.length-1)];
   var type = Math.random();
   var rand = Math.random();
-  if(type > 0.5) {
+  if(type < 0.3) {
     var temp = 20;
     if(rand > 0.5) temp += (rand * 10);
     else           temp -= (rand * 10);
@@ -28,19 +35,39 @@ function generate() {
     }
     tempList.push({"time": obj.data.lastupdate, "value": obj.data.value});
   } else {
-    var humidity = 0.5;
-    if(rand > 0.5) humidity += (rand);
-    else           humidity -= (rand);
+    if (type > 0.3 && type < 0.6) {
+      var maxUtil = parsedJson.features.filter(function (item) {
+        return item.properties.roomid === roomid
+      })[0].properties.data.filter(function (item) {
+        return item.name === "maxutilization"
+      })[0].value;
 
-    obj = {
-      "roomid": roomid,
-      "data": {
+      obj = {
+        "roomid": roomid,
+        "data": {
+          "name": "currentutilization",
+          "value": getRandomIntInclusive(0, maxUtil),
+          "lastupdate": date.toLocaleTimeString()
+        }
+      }
+      utilList.push({"time": obj.data.lastupdate, "value": obj.data.value});
+    }
+  else {
+
+      var humidity = 0.5;
+      if (rand > 0.5) humidity += (rand);
+      else           humidity -= (rand);
+
+      obj = {
+        "roomid": roomid,
+        "data": {
           "name": "humidity",
           "value": humidity,
           "lastupdate": date.toLocaleTimeString()
+        }
       }
+      humidityList.push({"time": obj.data.lastupdate, "value": obj.data.value});
     }
-    humidityList.push({"time": obj.data.lastupdate, "value": obj.data.value});
   }
   return obj;
 }
